54e3cfd5877fe6dc65f5bbcfaacdf5c67fea4339
FrostAvilaDev <avila_dev_emerson@proton.me>
2025-01-22 12:46:27 -0300

    update-config

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-import express from 'express';
+import express from "express";
 const app = express();
+app.use(express.json());
+
+
+// Method => GET, POST, PUT/PATCH, DELETE
+// Name => / - sempre no   plural
+// Callback functions => Onde EXECUTAMOS O BACKEND (LÓGICA, REGRA DE NEGÓCIO, ETC...)
 
 app.get("/", (req, res) => {
-    res.send("Helo World");
+  res.send("Hello World");
+});
+
+app.post("/users", (req, res) => {
+  const users = [];
+  const body = req.body;
+  users += body;
+  res.send(users);
 });
 
 app.listen(3000, () => {
-    console.log("Server started on port 3000");
+  console.log("Server started on port 3000");
 });
